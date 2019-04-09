8a15c7059c69a09b40eebc2e613351afe8d64841
Hannan <hannan9560@gmail.com>
2019-04-09 16:52:42 +0600

    Initial

---
+++ app/src/main/java/com/example/recyclerviewdemo/Hobby.kt
@@ -0,0 +1,27 @@
+package com.example.recyclerviewdemo
+
+data class Hobby(var title: String)
+
+object Supplier{
+    val hobbies = listOf(
+            Hobby("Swimming"),
+            Hobby("Reading"),
+            Hobby("Reading"),
+            Hobby("Reading"),
+            Hobby("Reading"),
+            Hobby("Reading"),
+            Hobby("Reading"),
+            Hobby("Reading"),
+            Hobby("Reading"),
+            Hobby("Reading"),
+            Hobby("Reading"),
+            Hobby("Reading"),
+            Hobby("Reading"),
+            Hobby("Reading"),
+            Hobby("Reading"),
+            Hobby("Reading"),
+            Hobby("Reading"),
+            Hobby("Reading"),
+            Hobby("Reading")
+        )
+}
---
+++ app/src/main/java/com/example/recyclerviewdemo/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.recyclerviewdemo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setupRecyclerView()
+    }
+
+    private fun setupRecyclerView() {
+
+        val layoutManager = LinearLayoutManager(this)
+        layoutManager.orientation = LinearLayoutManager.VERTICAL
+        recyclerView.layoutManager = layoutManager
+
+        val adapter = HobbiesAdapter(this, Supplier.hobbies)
+        recyclerView.adapter = adapter
+    }
+}
